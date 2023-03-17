69628e7f40d31b15b8cc3271f4bd65bfa8543599
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-03-17 19:28:45 +0100

    Add input & select elements

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -15,7 +15,13 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Current Weight</template>
 
-    Click inside this field and type in your weight in pounds or kilograms .
+    <input type="radio" id="lb" value="Pound"  />
+	  <label for="gi">kg</label>
+
+	  <input type="radio" id="kg" value="Kilograms"/>
+	  <label for="no gi">lb</label>
+    <br>
+    <input type="text" placeholder="Type in your current weight">
   </WelcomeItem>
 
   <WelcomeItem>
@@ -23,8 +29,24 @@ import SupportIcon from './icons/IconSupport.vue'
       <ToolingIcon />
     </template>
     <template #heading>Gi or No Gi</template>
+    
+    
 
+	  
+    
     Select either "Gi" or "No Gi" if you won't be wearing a Gi. If you plan to compete in a Gi Competition, choose the brand and size of your gi from the drop-down menu.
+    <br>
+    
+    <input type="radio" id="gi" value="Gi"/>
+	  <label for="gi">Gi</label>
+
+	  <input type="radio" id="no gi" value="No Gi"/>
+	  <label for="no gi">No Gi</label>
+    <br>
+    <!-- TODO: #3 Dropdown menu should only appear if the "Gi" radio button is selected -->
+    <select></select> <select></select>
+    
+    
   </WelcomeItem>
 
   <WelcomeItem>
@@ -32,8 +54,9 @@ import SupportIcon from './icons/IconSupport.vue'
       <EcosystemIcon />
     </template>
     <template #heading>Belt</template>
-
-   Click on the drop-down menu and select the brand and size of your belt.
+    Click on the drop-down menu and select the brand and size of your belt.
+    <br>
+    <select></select> <select></select>
   </WelcomeItem>
 
   <WelcomeItem>
@@ -41,8 +64,9 @@ import SupportIcon from './icons/IconSupport.vue'
       <CommunityIcon />
     </template>
     <template #heading>Competition</template>
-
     Click on the drop-down menu and select the BJJ competition you plan to participate in.
+    <br>
+    <select></select>
   </WelcomeItem>
 
   <WelcomeItem>
@@ -50,8 +74,9 @@ import SupportIcon from './icons/IconSupport.vue'
       <SupportIcon />
     </template>
     <template #heading>Weight Class</template>
-
-     Click on the drop-down menu and select the weight class you plan to compete in. The weight classes are based on the current BJJ tournaments.
+    Click on the drop-down menu and select the weight class you plan to compete in. The weight classes are based on the current BJJ tournaments.
+    <br>
+    <select></select>
   </WelcomeItem>
 
   <input type="submit" >
