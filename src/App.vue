a73e9e3bae429bdc6428220b07ef53afbacd86b4
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-03-18 22:49:47 +0100

    Update logo in header

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <header> 
     <img alt="Vue logo" class="logo" src="@/assets/body_scale.png" width="190" height="190" />
     
     <div class="wrapper">
