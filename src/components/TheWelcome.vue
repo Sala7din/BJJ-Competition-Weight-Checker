b00fbd043c29ec91d8d52c3c7cc0d2d9e4b66120
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-03-22 22:43:31 +0100

    Refactor: replace radio button with component

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,6 +5,7 @@ import WeightClassIcon from './icons/IconWeightClass.vue'
 import CurrentWeightIcon from './icons/IconCurrentWeight.vue'
 import GiIcon from './icons/IconGi.vue'
 import BeltIcon from './icons/IconBelt.vue'
+import RadioButton from './inputItems/radioButton.vue'
 </script>
 
 
@@ -15,12 +16,7 @@ import BeltIcon from './icons/IconBelt.vue'
       <GenderIcon />
     </template>
     <template #heading>Gender</template>
-
-    <input type="radio" id="gender" value="male"  />
-	  <label for="male">Male</label>
-
-	  <input type="radio" id="gender" value="female"/>
-	  <label for="female">Female</label>
+      <radio-button />
     
   </WelcomeItem>
 
@@ -35,11 +31,11 @@ import BeltIcon from './icons/IconBelt.vue'
             <option value="2">Light feather – above 57.5 kg and under 64 kg (127 – 141.6 lbs)</option>
             <option value="3">Feather – above 64kg and under 70 kg (141.6 – 154.6 lbs)</option>
             <option value="4">Light – above 70kg and under 76 kg (154.6 – 168 lbs)</option>
-            <option value="1">Middle – above 76 kg and under 82.3 kg (168 – 181.6 lbs)</option>
-            <option value="2">Medium heavy – above 82.3 kg and under 88.3 kg (181.6 – 195 lbs)</option>
-            <option value="3">Heavy – above 88.3 kg and under 94.3 kg (195 – 208 lbs)</option>
-            <option value="4">Super heavy – above 94.3 kg and under 100.5 kg (208 – 222 lbs)</option>
-            <option value="4">Ultra heavy – above 100.5 kg, no upper limit (222 lbs and up)</option>
+            <option value="5">Middle – above 76 kg and under 82.3 kg (168 – 181.6 lbs)</option>
+            <option value="6">Medium heavy – above 82.3 kg and under 88.3 kg (181.6 – 195 lbs)</option>
+            <option value="7">Heavy – above 88.3 kg and under 94.3 kg (195 – 208 lbs)</option>
+            <option value="8">Super heavy – above 94.3 kg and under 100.5 kg (208 – 222 lbs)</option>
+            <option value="9">Ultra heavy – above 100.5 kg, no upper limit (222 lbs and up)</option>
         </select>
     <!-- TODO: add drop-down for female weight classes and only render when female radio button is selected -->
   </WelcomeItem>
---
+++ src/components/inputItems/radioButton.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>{{ picked }}</div>
+
+	<input type="radio" id="male" value="Male" v-model="picked" />
+	<label for="male"><span>&#9794;</span></label>
+
+	<input type="radio" id="female" value="Female" v-model="picked" />
+	<label for="female"><span>&#9792;</span></label>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      picked: 'Male'
+    }
+  }
+}
+</script>
+  
