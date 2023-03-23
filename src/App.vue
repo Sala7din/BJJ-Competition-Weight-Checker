8f48c1dc88ddd55e502275bc5d14d01f5a0b643e
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-03-23 23:45:37 +0100

    Update font and background colors

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <img alt="Vue logo" class="logo" src="@/assets/body_scale.png" width="190" height="190" />
     <div class="wrapper">
-      <HelloWorld msg="IBJJF Competition Weight Checker" />
+      <HelloWorld msg="IBJJF Competition Weight Checker (unofficial)" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
