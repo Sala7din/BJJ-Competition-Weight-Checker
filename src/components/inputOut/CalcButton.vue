64a75994760ea35b5e3e88443caa56852d5eed57
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-03-24 15:52:01 +0100

    Add calaculator button component

---
+++ src/components/inputOut/CalcButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <button @click="checkWeight">Check Weight</button>
+  </template>
+  
+  <script>
+  export default {
+    name: 'WeightButton',
+    props: {
+      bodyWeight: {
+        type: Number,
+        default: 0
+      },
+      giWeight: {
+        type: Number,
+        default: 0
+      },
+      beltWeight: {
+        type: Number,
+        default: 0
+      },
+      selectedWeightClass: {
+        type: Number,
+        default: 0
+      }
+    },
+    methods: {
+      checkWeight() {
+        const totalWeight = this.userWeight + this.giWeight + this.beltWeight;
+        if (totalWeight <= this.weightClass) {
+          alert('You are within the weight limit!');
+        } else {
+          alert('You are over the weight limit!');
+        }
+      }
+    }
+  }
+  </script>
+  
