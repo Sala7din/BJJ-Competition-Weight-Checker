5256f1b38da26f907ee11735fa556571b84fd811
Sala7din <17114499+Sala7din@users.noreply.github.com>
2023-03-30 14:33:03 +0200

    Add content to About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Our app is designed for Brazilian Jiu-Jitsu enthusiasts participating in IBJJF Gi competitions. It allows you to easily check your weight for your desired weight class by inputting your current body weight, gi weight, and belt weight, selecting your gender and weight class from a list of IBJJF Gi Weight Classes. The app will then calculate whether you meet the weight requirements for your desired weight class and let you know if adjustments are necessary. It's easy to use for both beginners and seasoned athletes.</h1>
   </div>
 </template>
 
@@ -10,6 +10,7 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    font-size: small;
   }
 }
 </style>
